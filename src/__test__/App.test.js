2d1053c71301445fbaae08c661f6cb5792074a4e
Dave Bratby <dbratby1992@gmail.com>
2023-06-08 21:13:16 +0100

    created test components data and styles folders. Also created App.css for styling as well

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../Components/App';
 
 test('renders Weather App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Weather App/i);
   expect(linkElement).toBeInTheDocument();
 });
