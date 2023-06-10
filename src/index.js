65f7f3ee5af444e766a9ff1750886f0443684f44
Dave Bratby <dbratby1992@gmail.com>
2023-06-10 13:01:26 +0100

    added import react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Components/App';
-import forecast from './data/forecast.json'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./Styles/index.css";
+import App from "./Components/App";
+import forecast from "./data/forecast.json";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App location={forecast.location} />
+  </React.StrictMode>
 );
