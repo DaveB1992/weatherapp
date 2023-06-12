7c4317c8a04cf5f0e3d94ea3dee0e7e52de56000
Dave Bratby <dbratby1992@gmail.com>
2023-06-12 11:58:14 +0100

    Created a test for the App to make sure it is rendering properly

--- src/__test__/components/LocationDetails.test.js
+++ src/__test__/components/LocationDetails.test.js
@@ -1,6 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import App from "../../Components/App";
+import forecast from "../../data/forecast.json";
 import LocationDetails from "../../Components/LocationDetails";
+
+describe("App", () => {
+  test("renders App component correctly", () => {
+    render(<App location={forecast.location} />);
+    const h1Element = screen.getByText(/Manchester, UK/i);
+    expect(h1Element).toBeInTheDocument();
+  });
+});
 
 describe("LocationDetails", () => {
   it("render the correct city and location props", () => {
