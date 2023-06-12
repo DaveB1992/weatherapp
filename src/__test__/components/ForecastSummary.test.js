c8b7b8708f6bccdc645c0e549d1032fa94e4dad2
Dave Bratby <dbratby1992@gmail.com>
2023-06-12 16:48:40 +0100

    created the test to make sure the values are passing correctly for the props

--- src/__test__/components/ForecastSummary.test.js
+++ src/__test__/components/ForecastSummary.test.js
@@ -5,7 +5,7 @@ import ForecastSummary from "../../Components/ForecastSummary";
 describe("ForecastSummary", () => {
   const validProps = {
     date: 1111111,
-    description: "Stub Description",
+    description: "Stub description",
     icon: "stubIcon",
     temperature: {
       min: 12,
@@ -13,7 +13,7 @@ describe("ForecastSummary", () => {
     },
   };
 
-  it("renders", () => {
+  it("renders correctly", () => {
     const { asFragment } = render(
       <ForecastSummary
         date={validProps.date}
@@ -22,7 +22,28 @@ describe("ForecastSummary", () => {
         temperature={validProps.temperature}
       />
     );
-
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("render correct values for props", () => {
+    const { getByText, getByTestId } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+    expect(getByText("1111111")).toHaveClass("forecast-summary__date");
+    expect(getByText("Stub description")).toHaveClass(
+      "forecast-summary__description"
+    );
+
+    expect(getByText("1111111")).toHaveClass("forecast-summary__date");
+    expect(getByText("Stub description")).toHaveClass(
+      "forecast-summary__description"
+    );
+    expect(getByTestId("forecast-icon")).toHaveClass("forecast-summary__icon");
+    expect(getByText("22°C")).toHaveClass("forecast-summary__temperature");
   });
 });
