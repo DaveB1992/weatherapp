aadcb4e9323dd19720689be2272efbec729f1731
Dave Bratby <dbratby1992@gmail.com>
2023-06-12 15:45:43 +0100

    created a test for forecast summary to make sure it is rendering the props for component in ForecastSummary.js

--- src/__test__/components/ForecastSummary.test.js
+++ src/__test__/components/ForecastSummary.test.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../Components/ForecastSummary";
 
 describe("ForecastSummary", () => {
-  it("renders", () => {});
+  const validProps = {
+    date: 1111111,
+    description: "Stub Description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+
+  it("renders", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
