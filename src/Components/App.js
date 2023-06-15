da614939f0a168721e8390695b8ea74ecfa7b819
Dave Bratby <dbratby1992@gmail.com>
2023-06-15 10:46:07 +0100

    imported css into the file

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
+import "../Styles/App.css";
 
 function App(props) {
   const { forecasts, location } = props;
   return (
-    <div className="App">
+    <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
       <ForecastSummaries forecasts={forecasts} />
     </div>
