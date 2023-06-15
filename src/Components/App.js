fb06c017038d40ad70932592e1baf63f98b5d4ba
Dave Bratby <dbratby1992@gmail.com>
2023-06-15 14:08:24 +0100

    imported ForecastDetails

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
+import ForecastDetails from "./ForecastDetails";
 import "../Styles/App.css";
 
 function App(props) {
-  const { forecasts, location } = props;
+  const { forecasts, location, forecast } = props;
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
       <ForecastSummaries forecasts={forecasts} />
+      <ForecastDetails forecast={forecast} />
     </div>
   );
 }
