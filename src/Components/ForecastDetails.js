8ed5c10a94264a6035da9458d411c98802402460
Dave Bratby <dbratby1992@gmail.com>
2023-06-15 14:07:45 +0100

    created the Details component

--- src/Components/ForecastDetails.js
+++ src/Components/ForecastDetails.js
@@ -1,1 +1,26 @@
 import React from "react";
+
+function ForecastDetails({ forecast }) {
+  const { date, humidity, temperature, wind } = forecast;
+  const formattedDate = new Date(date).toDateString();
+
+  return (
+    <div className="forecast-details">
+      <div className="forecast-details__date">{formattedDate}</div>
+      <div className="forecast-details__min-temp">
+        <p>Min temperature: </p> {temperature.min} &deg;C
+      </div>
+      <div className="forecast-details__max-temp">
+        <p>Max temperature: </p> {temperature.max} &deg;C
+      </div>
+      <div className="forecast-details__humidity">
+        <p>Humidity:</p> {humidity} %
+      </div>
+      <div className="forecast-details__wind">
+        <p>Wind Speed: </p> {wind.speed} mph
+      </div>
+    </div>
+  );
+}
+
+export default ForecastDetails;
