8a105dabcd6b34d96237fc4bdf49d2b6bb2c621f
Dave Bratby <dbratby1992@gmail.com>
2023-06-15 15:46:26 +0100

    created the testing file and the first test for the Forecast details

---
+++ src/__test__/components/ForecastDetails.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import render from "@testing-library/react";
+import ForecastDetails from "../../Components/ForecastDetails";
+
+describe("ForecastDetails", () => {
+  const validProps = {
+    date: 11111111,
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+    humidity: 15,
+    wind: {
+      speed: 10,
+      direction: "n",
+    },
+  };
+  it("renders correctly", () => {
+    const { asFragment } = render(<ForecastDetails forecast={validProps} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
