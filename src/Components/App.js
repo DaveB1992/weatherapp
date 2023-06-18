f4ac1088cf860343132ac0db789e8e4b55bd76be
Dave Bratby <dbratby1992@gmail.com>
2023-06-18 11:42:47 +0100

    added the onForecastSelect prop

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import "../Styles/App.css";
 
 function App(props) {
-  const { forecasts, location, forecast } = props;
+  const { forecasts, location } = props;
+  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate
+  );
+
+  const handleForecastSelect = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
-      <ForecastSummaries forecasts={forecasts} />
-      <ForecastDetails forecast={forecast} />
+      <ForecastSummaries
+        forecasts={forecasts}
+        onForecastSelect={handleForecastSelect}
+      />
+      <ForecastDetails forecast={selectedForecast} />
     </div>
   );
 }
