e553cd9fb07a39f994c6d5cd5a53d5c4dbce93cb
Dave Bratby <dbratby1992@gmail.com>
2023-06-20 20:46:15 +0100

    removed package.json and the props passing into app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Styles/index.css";
 import App from "./Components/App";
-import forecast from "./data/forecast.json";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App location={forecast.location} forecasts={forecast.forecasts} />
+    <App />
   </React.StrictMode>
 );
